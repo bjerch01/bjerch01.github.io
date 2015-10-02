8120edc9c56629df60409dad52648489c9f79bc7
bjerch01 <bjerch01@luther.edu>
2015-10-02 12:16:18 -0500

    Update to-do3.js

--- to-do3.js
+++ to-do3.js
@@ -1,1 +1,5 @@
 
+<input type="radio" name="group1" value="Homework">Homework<br />
+<input type="radio" name="group1" value="Clean">Clean<br />
+<input type="radio" name="group1" value="Eat">Eat<br />
+
